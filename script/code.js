067ceb15554cf44815db9877a19622cfba29a4c3
joelmukanya <joel@lifechoices.co.za>
2022-05-21 15:01:30 +0200

    Updating testimonial section

--- script/code.js
+++ script/code.js
@@ -1,2 +1,12 @@
 // Fetch the currect year
 document.getElementById("currentYear").innerText = new Date().getFullYear();
+
+// Updating the width of the progress bar
+updateWidth = (progress, value)=>{
+    // round the value
+    value = Math.round(value);
+    // To change the width of the progress-bar
+    progress.querySelector(".progress").style.width = value;
+    // TO change the width of the span -> text in percentage
+    progress.querySelector(".progress-contents").textContent = '${value}%';
+}
