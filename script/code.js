b62b7b0255ac6b9765e59e7ceb7501e88df6bbfe
joelmukanya <joel@lifechoices.co.za>
2022-07-02 10:50:40 +0200

    Changes

--- script/code.js
+++ script/code.js
@@ -49,12 +49,14 @@ let profile_urls = [
 
 (function loadImage() {
     let imgContainer = 
-    document.querySelector('.img-container');
+    document.querySelector('.carousel-inner');
     imgContainer.innerHTML = '';
     // Loop through an array
     profile_urls.forEach( (item)=> {
         imgContainer.innerHTML += `
-        <img class='img-fluid' src='${item}' alt='Profile' >" 
+        <div class="carousel-item active">
+            <img src='${item}' class="d-block w-100" alt="Profile">
+        </div>
     `;
     });
 })();
